0e05149f04d787e757c2f8e2f6b75ccbf7397262
unknown <joao paulo barroso>
2022-06-08 22:52:01 -0300

    ajustes

--- Manipulacao_de_Dados/exercicio01.py
+++ Manipulacao_de_Dados/exercicio01.py
@@ -1,10 +1,13 @@
 import json
-from pathlib import Path
+import os
 
+cwd = os.getcwd()
+print(cwd)
 def printJson():
-    with open(r"../arquivos_json/partida.json", encoding="utf-8") as json_normal:
+    with open("./../arquivos_json/partida.json", encoding="utf-8") as json_normal:
         json_manipulavel = json.load(json_normal)
         return json_manipulavel
 
 json_retornado = printJson()
 print(json_retornado)
+
